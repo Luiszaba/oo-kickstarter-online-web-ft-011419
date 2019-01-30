3f9c0917e4d2044be61f682abe0dc2474ac6236c
Luis Zabala <luis.zabala076@gmail.com>
2019-01-30 18:38:13 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+  
+  @@all = []
+  
+  attr_accessor :name, :backed_projects
+  attr_reader :backers
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    project = Project.new
+    @backed_projects << project
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+
+
+@@all = []
+
+attr_accessor :title, :backers
+
+def initialize(title)
+  @title = title
+  @backers = []
+end
+
+end
